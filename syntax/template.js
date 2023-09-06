b7aa245250414a925db31f6c9ce6eedd29d153ff
chh4031 <chh4031@gmail.com>
2023-09-06 19:47:46 +0900

    syntax example

---
+++ syntax/template.js
@@ -0,0 +1,9 @@
+var name = "ed";
+
+var letter = "MSI " + name + "\nCare offers free extended warranty to protect your MSI products past their original product warranties. Just register your product and fill in a short questionnaire to enjoy a 3-month extension on your product warranty and avoid unexpected repair costs for much longer at no additional cost to you! The service only applies to laptops, desktops, all-in-one pcs, and monitors that are still within their respective commercial warranty period."
+var a = `asdf${name}
+
+asdadfsdFSddfsdf`
+console.log(a);
+
+// ``이용법, templete litarial
