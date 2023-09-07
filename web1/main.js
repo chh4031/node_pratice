47d798501cbe3d9fa9f5c6de2f9b31842398400f
chh4031 <chh4031@gmail.com>
2023-09-07 16:15:01 +0900

    코드 정리

--- web1/main.js
+++ web1/main.js
@@ -1,7 +1,7 @@
-let http = require('http');
-let fs = require('fs');
-let url = require('url');
-const { template } = require('underscore');
+let http = require("http");
+let fs = require("fs");
+let url = require("url");
+const { template } = require("underscore");
 
 let templateHTML = (title, list, body) => {
   return `
@@ -17,51 +17,56 @@ let templateHTML = (title, list, body) => {
   ${body}
 </body>
 </html>`;
-}
+};
 
 let templeteList = (filelist) => {
-  let list = '<ul>';
-        let i = 0;
-        while(i < filelist.length){
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list+`</ul>`;
-        return list
-}
+  let list = "<ul>";
+  let i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + `</ul>`;
+  return list;
+};
 
-let app = http.createServer(function(request,response){
-    let _url = request.url;
-    let queryData = url.parse(_url, true).query;
-    let pathname = url.parse(_url, true).pathname;
+let app = http.createServer(function (request, response) {
+  let _url = request.url;
+  let queryData = url.parse(_url, true).query;
+  let pathname = url.parse(_url, true).pathname;
 
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-
-        fs.readdir('../data', (error, filelist)=>{
-          let title = "Welcome";
-          let description = "Hello, Node js";
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("../data", (error, filelist) => {
+        let title = "Welcome";
+        let description = "Hello, Node js";
+        let list = templeteList(filelist);
+        let template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("../data", (error, filelist) => {
+        fs.readFile(`../data/${queryData.id}`, "utf8", (err, description) => {
+          let title = queryData.id;
           let list = templeteList(filelist);
-          let template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          let template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
           response.writeHead(200);
           response.end(template);
         });
-
-      }else{
-
-        fs.readdir('../data', (error, filelist)=>{
-          fs.readFile(`../data/${queryData.id}`, 'utf8', (err, description)=>{
-            let title = queryData.id;
-            let list = templeteList(filelist);
-            let template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-            response.writeHead(200);
-            response.end(template);
-    });
-    });
-      }
-    }else{
-      response.writeHead(404);
-      response.end('Not found');
+      });
     }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(3000);
